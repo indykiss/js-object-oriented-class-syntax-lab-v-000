44e262b99094e87865f78ce24e229be25ca92991
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-04-28 23:10:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,24 +4,24 @@ function BoardMember() {
 
   veto(){
   return "No, I must disagree";
-}
+  }
 
-approve(){
-  return "You can do that!";
-}
+  approve(){
+    return "You can do that!";
+    }
 
-doCharity(){
-  return "I like to help people.";
-}
+  doCharity(){
+    return "I like to help people.";
+    }
 
-releasePressStatement(){
-  return "You will see great things from Scuber.";
-}
+  releasePressStatement(){
+    return "You will see great things from Scuber.";
+    }
 
-sayHi(){
-  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
-}
-}
+  sayHi(){
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`;
+    }
+    }
 
 
 }
